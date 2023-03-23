be3f34e806af45a2f0610a9ee9fc604b4acc7922
Marvin Sio <marvinbcit@gmail.com>
2023-03-23 01:08:22 -0700

    added data to be loaded

--- app/data/launchtimes.js
+++ app/data/launchtimes.js
@@ -2,16 +2,71 @@
     {
         "Mission": "Tropospheric Emissions Monitoring of Pollution (TEMPO)",
         "Date": "April 2023",
-        "description": "NASA's first Earth Venture Instrument mission, TEMPO will measure the pollution over North America, from Mexico City to the Canadian oil sands, and from the Atlantic to the Pacific, hourly and at high spatial resolution. TEMPO will be the first space-based instrument to monitor air pollutants hourly across the North American continent during daytime."
+        "Priority": "High",
+        "Astronaut": "Neil Armstrong",
+        "description": "NASA's first Earth Venture Instrument mission, TEMPO will measure the pollution over North America, from Mexico City."
     },
     {
-        "Mission": "NASA's Boeing Crew Flight Test",
+        "Mission": "Boeing Crew Flight Test",
         "Date": "April 2023",
-        "description": "The Crew Flight Test will demonstrate the ability of Starliner and the United Launch Alliance’s Atlas V rocket to safely carry astronauts to and from the International Space Station. Following a successful test flight with astronauts, NASA will begin the final process of certifying the Starliner spacecraft and systems for regular crew rotation flights to the space station."
+        "Priority": "Medium",
+        "Astronaut": "Georgy Dobrovolsky",
+        "description": "The Crew Flight Test will demonstrate the ability of Starliner."
     },
     {
         "Mission": "Astrobotic Commercial Lunar Payload Services",
         "Date": "May 4, 2023",
-        "description": "A suite of robotic NASA payloads will be sent to the lunar surface as part of a Commercial Lunar Payload Services delivery, in this case by Astrobotic Technology of Pittsburgh. Landing will take place in the following weeks."
+        "Priority": "Low",
+        "Astronaut": "Eugene Cernan",
+        "description": "A suite of robotic NASA payloads will be sent to the lunar surface as part of a Commercial Lunar Payload Services delivery, in this case by Astrobotic Technology of Pittsburgh."
+    },
+    {
+        "Mission": "XRISM",
+        "Date": "2023",
+        "Priority": "Low",
+        "Astronaut": "Fred Haise",
+        "description": "The X-ray Imaging and Spectroscopy Mission is a collaborative mission between the Japan Aerospace Exploration Agency and NASA."
+    },
+    {
+        "Mission": "X-57 First Flight",
+        "Date": "2023",
+        "Priority": "Low",
+        "Astronaut": "Harrison Schmitt",
+        "description": "The first flight of NASA's X-57, a small, experimental airplane powered by electricity."
+    },
+    {
+        "Mission": "Quesst",
+        "Date": "2023",
+        "Priority": "Low",
+        "Astronaut": "Viktor Patsayev",
+        "description": "The first flight of the Quesst quiet supersonic aircraft will take place out of Lockheed flight facilities in Palmdale, California."
+    },
+    {
+        "Mission": "Intuitive Machines Commercial Payload Lunar Services",
+        "Date": "June 2023",
+        "Priority": "Medium",
+        "Astronaut": "Vladislav Volkov",
+        "description": "A suite of robotic NASA payloads will be sent to the lunar surface as part of a Commercial Lunar Payload Services delivery."
+    },
+    {
+        "Mission": "EUCLID",
+        "Date": "July 2023",
+        "Priority": "Low",
+        "Astronaut": "Yang Liwei",
+        "description": "Euclid is designed to explore the composition and evolution of the dark universe."
+    },
+    {
+        "Mission": "OSIRIS-REx",
+        "Date": "September 24, 2023",
+        "Priority": "High",
+        "Astronaut": "Yi So-Yeon",
+        "description": "A capsule containing pieces of asteroid Bennu will separate from the OSIRIS-REx spacecraft and enter Earth’s atmosphere."
+    },
+    {
+        "Mission": "Psyche",
+        "Date": "October 2023",
+        "Priority": "Medium",
+        "Astronaut": "Jerry Ross",
+        "description": "Psyche will travel across the solar system to an asteroid of the same name, which has unusually high metal content."
     }
 ]
